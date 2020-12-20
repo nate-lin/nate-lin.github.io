d0a0dd9d21e3fd7cc9f0c30428b9090d111a5541
nate-lin <linchunyan335@163.com>
2020-12-20 17:02:56 +0800

    Site updated: 2020-12-20 17:02:52

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/17/nDNwt8Hm1iW4sRg.png");
+        $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/20/BZhvzONd8lgoFAI.png");
         document.title = 'ヾ(❀╹◡╹)ﾉ~我藏好了哦';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/17/nDNwt8Hm1iW4sRg.png");
+        $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/20/BZhvzONd8lgoFAI.png");
         document.title = '（*＾ワ＾*）被你发现啦' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
